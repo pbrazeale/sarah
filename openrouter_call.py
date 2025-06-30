f84763165c7f8fcdf596f67a8c024cc5c8195b2e
pbrazeale <pbrazeale@gmail.com>
2025-06-30 08:56:21 -0500

    0.4.1 Update openrouter_call

--- openrouter_call.py
+++ openrouter_call.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+from prompts.prompt import llm_text
 
 load_dotenv()
 openrouter_api_key = os.environ.get("OPENROUTER_API_KEY")
@@ -11,7 +12,7 @@ client = OpenAI(
 )
 
 completion = client.chat.completions.create(
-  model="${Model.GPT_4_Omni}",
+  model="openai/gpt-4.1",
   # extra_headers={
   #   "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
   #   "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
@@ -26,7 +27,10 @@ completion = client.chat.completions.create(
   messages=[
     {
       "role": "user",
-      "content": "Say this is a test",
+      "content": [
+        "type": "text",
+        "text": llm_text
+      ]
     },
   ],
 )
