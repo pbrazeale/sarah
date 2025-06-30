1917f9c7a557b460bfe5340e0d455d1b1dae6e45
pbrazeale <pbrazeale@gmail.com>
2025-06-30 15:31:36 -0500

    0.8.0 prompt.py

--- prompts/prompt.py
+++ prompts/prompt.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
-from guidelines import LINE_EDITING_GUIDELINES, LINE_EDITING_EXAMPLE, DEVELOPMENTAL_EDITING_GUIDELINES, DEVELOPMENTAL_EDITING_EXAMPLE
+from guidelines import LINE_EDITING_GUIDELINES, LINE_EDITING_EXAMPLE, DEVELOPMENTAL_EDITING_GUIDELINES, DEVELOPMENTAL_EDITING_EXAMPLE, BEATSHEET_ANALYSIS_GUIDELINE, BEATSHEET_ANALYSIS_EXAMPLE
 
 load_dotenv()
 aconite_cafe_code = os.environ.get("ACONITE_CAFE_CODE")
 
 def get_llm_text(objective_selection):
-    objectives = ["Line Edit", "Developmental Edit"]
+    objectives = ["Line Edit", "Developmental Edit", "Beatsheet Analysis"]
     try:
         objective = objectives[objective_selection]
     except IndexError:
@@ -13,25 +13,24 @@ def get_llm_text(objective_selection):
 
     if objective == "Line Edit":
         operations = f"""
-        - Read the manuscript
-        - Analyze the manuscript for PARAMETERS consistency
-            - Provide a score from 18 to 99 based on how well the original manuscript aligned with the PARAMETERS
-        - Edit the manuscript according the PARAMETERS with minimal rewrites following the {LINE_EDITING_GUIDELINES}
-        - Save the edited manuscript to a new file named `'SAMETITLE'_edited.md`
-        
+        {LINE_EDITING_GUIDELINES}
+
         Follow the desired format: 
         {LINE_EDITING_EXAMPLE}
         """
     elif objective == "Developmental Edit":
         operations = f"""
-        - Read the manuscript
-        - Analyze the manuscript for PARAMETERS consistency
-        - Review the manuscript according the PARAMETERS following the {DEVELOPMENTAL_EDITING_GUIDELINES}
-            - Write the feedback in a positive tone with actionable critiques for the author to implement  
-        - Save the feedback to a new file named `'MANUSCRIPTTITLE'_feedack.md`
-        
-        Follow the desired format 
+        {DEVELOPMENTAL_EDITING_GUIDELINES}
+
+        Follow the desired format: 
         {DEVELOPMENTAL_EDITING_EXAMPLE}
+        """
+    elif objective == "Beatsheet Analysis":
+        operations = f"""
+        {BEATSHEET_ANALYSIS_GUIDELINE}
+
+        Follow the desired format: 
+        {BEATSHEET_ANALYSIS_EXAMPLE}
         """
 
     llm_text = f"""
