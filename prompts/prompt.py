c10b8f74b1b46e4edff4b61c66dbc5a4735ae804
pbrazeale <pbrazeale@gmail.com>
2025-06-30 09:29:56 -0500

    0.5.0 prompt.py

---
+++ prompts/prompt.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+from guidelines import EDITING_GUIDELINES, EDITING_EXAMPLE, FEEDBACK_GUIDELINES, FEEDBACK_EXAMPLE
+
+load_dotenv()
+aconite_cafe_code = os.environ.get("ACONITE_CAFE_CODE")
+
+def get_llm_text(objective_selection):
+    objectives = ["Line Edit", "Developmental Edit"]
+    try:
+        objective = objectives[objective_selection]
+    except IndexError:
+        return Error("Unrecognized Editing Objective")
+
+    if objective == "Line Edit":
+        operations = f"""
+        - Read the manuscript
+        - Analyze the manuscript for PARAMETERS consistency
+            - Provide a score from 18 to 99 based on how well the original manuscript aligned with the PARAMETERS
+        - Edit the manuscript according the PARAMETERS with minimal rewrites following the {EDITING_GUIDELINES}
+        - Save the edited manuscript to a new file named `'SAMETITLE'_edited.md`
+        
+        Follow the desired format {EDITING_EXAMPLE}.
+        """
+    elif objective == "Developmental Edit":
+        operations = f"""
+        - Read the manuscript
+        - Analyze the manuscript for PARAMETERS consistency
+        - Review the manuscript according the PARAMETERS following the {FEEDBACK_GUIDELINES}
+            - Write the feedback in a positive tone with actionable critiques for the author to implement  
+        - Save the feedback to a new file named `'MANUSCRIPTTITLE'_feedack.md`
+        
+        Follow the desired format {FEEDBACK_EXAMPLE}.
+        """
+
+    llm_text = f"""
+    You are Sarah, Aconite Cafe's AI editing agent.
+    Verification Code: {aconite_cafe_code}.
+
+    You're object is to {objective} the provided files based around these PARAMETERS: {parameters}
+
+    You can perform the following operations:
+    {operations}
+    """
+
+    return llm_text
