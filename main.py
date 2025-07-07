03ede4ed01e48568aee32eafe4acc73570f25fa7
pbrazeale <pbrazeale@gmail.com>
2025-07-07 09:28:57 -0500

    0.9.0 chapter_processing loop

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from oprouter_call import call_openrouter
+from functions.openrouter_call import call_openrouter
 
 def main():
-    call_openrouter(0)
+    chapter_processing = False
+    while chapter_processing == False:
+        chapter_processing_request = input('Have you palced your manuscript in ./working_dir/import? "Yes", "No"\nOption: ')
+        chapter_processing_request.lower()
+        if chapter_processing_request == "yes":
+            chapter_processing = True
+    
+
+
+    # operation_request = input("What ")
+    # call_openrouter()
+
+
+if __name__ == "__main__":
+    main()
