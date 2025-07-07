11fdaf3c8571672aee96a97a46c53aa19e132aa6
pbrazeale <pbrazeale@gmail.com>
2025-07-07 10:40:02 -0500

    0.13.0 main.py

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from functions.process_chapter import process_chapter
 
 def main():
     chapter_processing = False
-    while chapter_processing == False:
+    while not chapter_processing:
         chapter_processing_request = input('Have you palced your manuscript in ./working_dir/import? "Yes", "No"\nOption: ')
-        chapter_processing_request.lower()
+        chapter_processing_request = chapter_processing_request.lower()
         if chapter_processing_request == "yes":
             chapter_processing = True
     
-    process_chapter()
+    manuscript_file, chapter_files = process_chapter()
+
+    if manuscript_file is None:
+        print("No manuscript processed. Exiting.")
+        return
 
     create_developmental_edit = False
-    while create_developmental_edit == False:
+    while not create_developmental_edit:
         beat_sheet_request = input('Would you like to create a developmental edit report? "Yes", "No"\nOption: ')
-        beat_sheet_request.lower()
+        beat_sheet_request = beat_sheet_request.lower()
         if beat_sheet_request == "yes":
             create_developmental_edit = True
-            call_openrouter(0)
+            call_openrouter(0, manuscript_file)
+            call_openrouter(1, manuscript_file)
 
 
 if __name__ == "__main__":
