6f305e91454996d91a45a7cfa64a1de66f020f69
pbrazeale <pbrazeale@gmail.com>
2025-07-14 10:13:23 -0500

    4.2.1 TITLE

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 # Navigation Panel
 pg = st.navigation([st.Page("import.py"), st.Page("export.py")])
 pg.run()
+
+# TITLE
+st.title("SARAH: Story Analysis & Revision AI Helper")
